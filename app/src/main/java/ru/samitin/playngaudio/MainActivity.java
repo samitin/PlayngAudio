3dd6bd8cc5fc0c3612bbe95db17bda5e8cfea3c1
samitin <samitin.e@gmail.com>
2021-05-26 16:18:29 +1100

    Задание 19: Используем аудио в приложении

--- app/src/main/java/ru/samitin/playngaudio/MainActivity.java
+++ app/src/main/java/ru/samitin/playngaudio/MainActivity.java
@@ -4,15 +4,33 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private boolean isPlay=false;
+    MediaPlayer mediaPlayer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        MediaPlayer mediaPlayer=MediaPlayer.create(getApplicationContext(),R.raw.stuff);
-        mediaPlayer.start();
+        mediaPlayer=MediaPlayer.create(getApplicationContext(),R.raw.stuff);
+
+
+        Button buttonPlayPause=findViewById(R.id.buttonPlayPause);
+        buttonPlayPause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                if(mediaPlayer.isPlaying()) {
+                    mediaPlayer.pause();
+                    buttonPlayPause.setText(getText(R.string.play));
+                }else {
+                    mediaPlayer.start();
+                    buttonPlayPause.setText(getText(R.string.pause));
+                }
+            }
+        });
     }
 }
